91f2b518cdc4b62e780923685c0cb4feff93c0ba
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2022-05-11 18:58:01 +0200

    Prise en main et essais
    Ajout des premiers éléments pour tester

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonInput
+} from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
+        <IonSegment
+          mode="ios"
+          onIonChange={(e) => console.log("Segment selected", e.detail.value)}
+        >
+          <IonSegmentButton value="afaire">
+            <IonLabel>À faire</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="tous">
+            <IonLabel>Tous</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          <IonItem>
+            <IonLabel>Pokémon Yellow</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mega Man X</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>The Legend of Zelda</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Pac-Man</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Super Mario World</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
+      <footer>
+        <IonItem>
+          <IonInput
+            /*value={text}*/
+            placeholder="Entrer une tâche"
+            /*onIonChange={(e) => setText(e.detail.value!)}*/
+            clearInput
+          ></IonInput>
+        </IonItem>
+      </footer>
     </IonPage>
   );
 };
